39ee085882398ac7bcc9db38252f5cca872ba62c
baghira <sandeep.gupta@tokopedia.com>
2021-05-14 22:44:05 +0530

    Zxing scanner integrated for sdk < 24 and confirmed scanned result in toast.

--- app/src/main/java/com/test/librarytimer/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/test/librarytimer/presentation/ui/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.test.librarytimer.presentation.ui
 
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.ViewFlipper
 import androidx.lifecycle.ViewModelProvider
+import com.google.zxing.integration.android.IntentIntegrator
 import com.test.librarytimer.R
 import com.test.librarytimer.presentation.viewmodel.EntryExitViewModel
 import java.text.SimpleDateFormat
@@ -27,16 +30,25 @@ class MainActivity : AppCompatActivity() {
 
     private val timerFormatter = SimpleDateFormat("HH:mm:ss", Locale.ENGLISH)
 
+    // zxing
+    private lateinit var qrScan: IntentIntegrator
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViewsAndPreference()
+        initScanner()
         initTimeZone()
         setClickListener()
         if (!::viewModel.isInitialized)
             initViewModel()
         observeLiveDataForTimerUpdate()
         viewModel.startTimer(System.currentTimeMillis())
+    }
+
+    private fun initScanner() {
+        qrScan = IntentIntegrator(this)
     }
 
     private fun observeLiveDataForTimerUpdate() {
@@ -55,6 +67,7 @@ class MainActivity : AppCompatActivity() {
     private fun setClickListener() {
         btn.setOnClickListener {
             // start scanning
+            qrScan.initiateScan()
         }
     }
 
@@ -73,6 +86,19 @@ class MainActivity : AppCompatActivity() {
 
         prefs = getSharedPreferences("prefs", MODE_PRIVATE)
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,"scanned result : " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 
     override fun onStop() {
